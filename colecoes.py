ae2e7edaf83def1789a267068a190e75101776ec
Lívia Rocha <liviaantenucci@colegiopaulodetarso.com.br>
2025-10-08 16:10:18 +0000

    final

---
+++ colecoes.py
@@ -0,0 +1,9 @@
+tabuada_3 = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
+print("Lista: ", tabuada_3)
+print("-----------------------------------")
+print("Maior valor:", max(tabuada_3))
+print("Menor valor:", min(tabuada_3))
+print("Ordenado:", sorted(tabuada_3))
+print("Quantidade de Números:", len(tabuada_3))
+print("Média de Números:", sum(tabuada_3)/len(tabuada_3))
+print("-----------------------------------")
